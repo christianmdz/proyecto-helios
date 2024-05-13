12d76baa3e9dba5491a281dec8b8265da0342970
christianmdz <156136297+christianmdz@users.noreply.github.com>
2024-05-13 22:19:19 +0200

    Actualización

--- Frontend/src/components/Home/HomeBody.jsx
+++ Frontend/src/components/Home/HomeBody.jsx
@@ -1,8 +1,24 @@
 import React from "react"
 import circe from '../../assets/circe_test.jpeg'
+import { Box } from "@mui/material"
 
 export default function HomeBody() {
-    return (
-        <img src={circe} alt="Circe Spaceship" />
-    )
+  return (
+    <Box sx={{
+      backgroundColor: 'black',
+      display: 'flex',
+      justifyContent: 'center',
+      overflow: 'hidden',
+      width: '100vw',
+      height: '100vh',
+    }}>
+      <img src={circe} alt="Circe Spaceship"
+        style={{
+          objectFit: 'cover',
+          width: '100%',
+          height: '100%',
+        }}
+      />
+    </Box>
+  )
 }
--- Frontend/src/components/Home/HomeNav.jsx
+++ Frontend/src/components/Home/HomeNav.jsx
@@ -1,4 +1,5 @@
-import { AppBar, CssBaseline, Toolbar } from "@mui/material";
+import { AppBar, CssBaseline, Toolbar, Typography, Box } from "@mui/material";
+import { Button } from "bootstrap";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -6,10 +7,19 @@ export default function HomeNav() {
   return (
     <div>
       <CssBaseline />
-      <AppBar position="static" sx={{ backgroundColor: 'hsla(210, 14%, 7%, 0.7)' }}>
+      <AppBar sx={{ backgroundColor: 'hsla(210, 14%, 7%, 0.8)', backdropFilter: 'blur(8px)' }}>
         <Toolbar>
-          <p>HELIOS</p>
+        <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, fontFamily:'Dune' }}
+          >
+            HELIOS
+          </Typography>
         </Toolbar>
+        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Button>Login</Button>
+        </Box>
       </AppBar>
     </div>
   )
